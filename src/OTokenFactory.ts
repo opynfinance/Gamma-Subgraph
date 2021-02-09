afece3b49d03f5ca6e2d47218eb9961e6243eaa6
Anton Cheng <antonassocareer@gmail.com>
2021-02-09 18:01:48 +0800

    Add implementation address in oToken entity

--- src/OTokenFactory.ts
+++ src/OTokenFactory.ts
@@ -2,12 +2,20 @@ import { BigInt } from "@graphprotocol/graph-ts"
 import { OtokenCreated } from "../generated/OTokenFactory/OTokenFactory"
 import { OToken as OTokenSource } from "../generated/templates"
 import { OToken as TokenContract } from "../generated/templates/OToken/OToken"
-import { OToken } from "../generated/schema"
+import { AddressBook as AddressBookInterface } from '../generated/AddressBook/AddressBook'
+import { OTokenFactory as FactoryInterface } from '../generated/OTokenFactory/OTokenFactory'
+import { OToken, Factory } from "../generated/schema"
 
 export function handleOtokenCreated(event: OtokenCreated): void {
 
   // Start indeing the newly created OToken contract
   OTokenSource.create(event.params.tokenAddress)
+
+  // bind to the address that emit the event
+  let factoryContract = FactoryInterface.bind(event.address)
+  let addressBookAddress = factoryContract.addressBook()
+  let addressBookContract = AddressBookInterface.bind(addressBookAddress)
+  let implementation = addressBookContract.getOtokenImpl()
   
   // Create Otoken Entity
   let entity = new OToken(event.params.tokenAddress.toHex())
@@ -19,6 +27,7 @@ export function handleOtokenCreated(event: OtokenCreated): void {
   entity.isPut = event.params.isPut
   entity.expiryTimestamp = event.params.expiry
   entity.creator = event.params.creator
+  entity.implementation = implementation
 
   let contract = TokenContract.bind(event.params.tokenAddress)
   // Access state variables and functions by calling them
