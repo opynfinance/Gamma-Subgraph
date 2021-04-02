55e318e1b2d4a32a291947eedfb2707f6178f99d
Alexis Gauba <alexisgauba@gmail.com>
2021-04-01 20:05:49 -0700

    Add ropsten and 0x v4  (#22)
    * add config for ropsten and 0x v4
    
    * update ropsten config
    
    * try to add logging
    
    * allow 0x v3 and v4
    
    * support 0x v3 and v4
    
    * cleanup
    
    * build fixes
    
    * actual build fix

--- src/0xExchange.ts
+++ src/0xExchange.ts
@@ -1,12 +1,100 @@
-import { Fill } from '../generated/zxExchange/ZxExchange';
-import { Whitelist as WhitelistContract } from '../generated/Whitelist/Whitelist';
-import { AddressBook as AddressBookContract } from '../generated/AddressBook/AddressBook';
-import { FillOrder, Controller, OTokenTrade, ERC20 } from '../generated/schema';
-import { Address } from '@graphprotocol/graph-ts';
-import { checkERC20Entity } from './Whitelist';
-// import { updateBuyerPosition, updateSellerPosition } from './helper';
+import { LimitOrderFilled } from "../generated/zxExchangeV4/ZxExchangeV4"
+import { Fill } from "../generated/zxExchangeV3/ZxExchangeV3"
+import { Whitelist as WhitelistContract } from "../generated/Whitelist/Whitelist"
+import { AddressBook as AddressBookContract } from "../generated/AddressBook/AddressBook"
+import { FillOrderV4, FillOrderV3, Controller, OTokenTrade, ERC20 } from '../generated/schema'
+import { Address} from "@graphprotocol/graph-ts"
+// import { checkERC20Entity } from "./Whitelist"
+// import { updateBuyerPosition, updateSellerPosition, ZERO_ADDRESS } from './helper'
 
-export function handleFillOrder(event: Fill): void {
+export function handleFillOrderV4(event: LimitOrderFilled): void {
+  let id = event.params.orderHash.toHex() + '-' + event.transaction.hash.toHex()
+
+  let makerAssetAddr = event.params.makerToken
+  let takerAssetAddr = event.params.takerToken
+
+  // get addressBook address
+  let controller = Controller.load('1');
+
+  let makerAssetIsOToken = false;
+  let takerAssetIsOToken = false;
+
+  let addressBook = AddressBookContract.bind(Address.fromString(controller.addressBook.toHex()));
+  let whitelistAddr = addressBook.getWhitelist();
+  let whitelistContract = WhitelistContract.bind(whitelistAddr);
+
+    makerAssetIsOToken = whitelistContract.isWhitelistedOtoken(makerAssetAddr)
+    takerAssetIsOToken = whitelistContract.isWhitelistedOtoken(takerAssetAddr)
+  
+  if (!makerAssetIsOToken && !takerAssetIsOToken) return
+
+  // Record Fill event
+
+  let fill = new FillOrderV4(id);
+  fill.transactionHash = event.transaction.hash;
+  fill.timestamp = event.block.timestamp
+  fill.maker = event.params.maker
+  fill.taker = event.params.taker
+  fill.makerAmount = event.params.makerTokenFilledAmount
+  fill.takerAmount = event.params.takerTokenFilledAmount
+  fill.protocolFeePaid = event.params.protocolFeePaid
+  fill.makerAsset = makerAssetAddr.toHexString();
+  fill.takerAsset = takerAssetAddr.toHexString();
+  
+  fill.save()
+
+  // convert to an OTokenTrade entity
+  let trade = new OTokenTrade(id);
+  trade.exchange = 'ZeroX';
+  trade.timestamp = event.block.timestamp;
+  trade.transactionHash = event.transaction.hash;
+
+  let seller: Address;
+  let buyer: Address;
+
+  if (makerAssetIsOToken) {
+    // maker asset is oToken, maker is seller
+
+    let paymentToken = ERC20.load(takerAssetAddr.toHexString())
+    if (paymentToken == null) return
+
+    // checkERC20Entity(Address.fromString(takerAssetAddr));
+
+    seller = event.params.maker;
+    buyer = event.params.taker;
+
+    trade.oToken = makerAssetAddr.toHexString();
+    trade.oTokenAmount = event.params.makerTokenFilledAmount;
+
+    trade.paymentToken = takerAssetAddr.toHexString();
+    trade.paymentTokenAmount = event.params.takerTokenFilledAmount;
+  } else {
+    // taker asset is oToken. Taker is seller
+
+    let paymentToken = ERC20.load(makerAssetAddr.toHexString())
+    if (paymentToken == null) return
+    // checkERC20Entity(Address.fromString(makerAssetAddr));
+
+    seller = event.params.taker;
+    buyer = event.params.maker;
+
+    trade.oToken = takerAssetAddr.toHexString();
+    trade.oTokenAmount = event.params.takerTokenFilledAmount;
+
+    trade.paymentToken = makerAssetAddr.toHexString();
+    trade.paymentTokenAmount = event.params.makerTokenFilledAmount;
+  }
+
+  trade.seller = seller;
+  trade.buyer = buyer;
+
+  trade.save();
+
+  // updateBuyerPosition(buyer, trade.oToken, trade.oTokenAmount, id);
+  // updateSellerPosition(seller, trade.oToken, trade.oTokenAmount, id);
+}
+
+export function handleFillOrderV3(event: Fill): void {
   let id = event.params.orderHash.toHex() + '-' + event.transaction.hash.toHex();
 
   let makerAssetAddr = '0x' + event.params.makerAssetData.toHex().substr(-40);
@@ -29,7 +117,7 @@ export function handleFillOrder(event: Fill): void {
 
   // Record Fill event
 
-  let fill = new FillOrder(id);
+  let fill = new FillOrderV3(id);
   fill.transactionHash = event.transaction.hash;
   fill.timestamp = event.block.timestamp;
   fill.makerAddress = event.params.makerAddress;
